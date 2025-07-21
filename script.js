a216c596d9a82238338b3121fe82610c72a0ba81
Parashmani Awasthi <awasthiparash333@gmail.com>
2025-07-21 20:35:52 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Optional for smooth scroll in future
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
